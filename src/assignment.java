17761aed44990f126b305a935e9e9b234c137b42
Rohansingh287 <103259230+Rohansingh287@users.noreply.github.com>
2022-04-10 14:24:45 +0530

    renukasisodia

---
+++ src/assignment.java
@@ -0,0 +1,34 @@
+
+public class assignment {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		int b=30;
+		int c=20;
+		
+		System.out.println("hello");
+		System.out.println("rohan singh sisodiya");
+		System.out.println((a+b));
+
+		System.out.println((a=b));
+		System.out.println((b=c));
+
+
+
+		
+		
+		
+		
+		System.out.println("sum of two numbers");
+           
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/logicaloperators.java
@@ -0,0 +1,19 @@
+
+public class logicaloperators {
+
+	public static void main(String[] args) {
+		boolean a= true;
+	    boolean	b=false;
+		
+		
+		System.out.println("...logical operatora...a..a..a..a..");
+		System.out.println(a && b);
+		System.out.println(a || b);
+		System.out.println(!a);
+		System.out.println(!b);
+		
+
+
+	}
+
+}
